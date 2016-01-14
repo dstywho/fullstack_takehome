60e8e838fdeab423c624503f1f1851a4548d19ba
kangkyu <kangkyu1111@gmail.com>
2016-01-13 16:57:44 -0800

    add Histogram table

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,8 @@
 class DashboardController < ApplicationController
+
+  def index
+    @histogram = Video.all.each_with_object({}) do |video, h|
+      h.merge!({video.title => video.users.count})
+    end
+  end
 end
